8defc8c500bac1c1a27a5b2d89ec9f2e78fba407
liuzhentao <liuzhentao@motingzhi.com>
2023-09-15 15:13:46 +0800

    rounded库新增RoundedFrameLayout、RoundedLinearLayout、RoundedRelativeLayout、RoundedGridLayout、RoundedTableLayout等常用布局

---
+++ rounded/src/main/java/com/arc/fast/view/rounded/RoundedGridLayout.kt
@@ -0,0 +1,46 @@
+package com.arc.fast.view.rounded
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.GridLayout
+
+
+open class RoundedGridLayout : GridLayout, IRoundedView {
+    override var _config = RoundedViewConfig()
+    override var _temporarilyConfig: RoundedViewConfig? = null
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        initRoundedRadius(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context, attrs, defStyleAttr
+    ) {
+        initRoundedRadius(context, attrs)
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes) {
+        initRoundedRadius(context, attrs)
+    }
+
+//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+//        layoutWidth = measuredWidth
+//        layoutHeight = measuredHeight
+//        setMeasuredDimension(layoutWidth, layoutHeight)
+//    }
+
+    override fun draw(canvas: Canvas) {
+        onDrawBefore(canvas)
+        super.draw(canvas)
+        onDrawAfter(canvas)
+    }
+}
